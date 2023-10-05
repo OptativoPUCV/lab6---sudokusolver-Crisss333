ae5b5e344b7528a2c6b31a5ff6aaaf2c1610a7a1
Replit user <>
2023-10-05 13:00:04 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -63,6 +63,13 @@ int is_valid(Node* n){
                 return 0; // Número repetido en la columna.
             }
             col_check[j][num] = 1;
+
+            // Verificar submatrices de 3x3.
+            int submatrix_index = 3 * (i / 3) + (j / 3);
+            if (submatrix_check[submatrix_index][num] == 1) {
+                return 0; // Número repetido en la submatriz.
+            }
+            //submatrix_check[submatrix_index][num] = 1;
         }
     }
 
