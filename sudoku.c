fa6c6eb8f18debb0e354c6c764bfac5bf971c8fa
Replit user <>
2023-10-08 22:33:12 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -136,15 +136,15 @@ List* get_adj_nodes(Node* n){
                     }
                 }
                 // Se ha encontrado una casilla vacía, no es necesario buscar más.
-                if (found) {
-                    break;
-                }
+                //if (found) {
+                //    break;
+                //}
             }
         }
         // Se ha encontrado una casilla vacía, no es necesario buscar más.
-        //if (found) {
-        //    break;
-        //}
+        if (found) {
+            break;
+        }
     }            
     return list;
 }
