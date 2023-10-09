c5847b41aa727446219cf72b648c2d66ceb87224
Replit user <>
2023-10-09 00:33:12 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -248,7 +248,7 @@ Node* DFS(Node* initial, int* cont){
 }
 */
 
-/*
+
 int main( int argc, char *argv[] ){
 
   Node* initial= read_file("s12a.txt");;
@@ -260,5 +260,5 @@ int main( int argc, char *argv[] ){
 
   return 0;
 }
-*/
+
 
